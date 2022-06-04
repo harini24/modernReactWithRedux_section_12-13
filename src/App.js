1ba5ed13d11ef3c2daef53f1f7ef640eca993d9b
harini24 <harinisiva1997@gmail.com>
2022-06-04 11:07:22 +0530

    accordions displayed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Accordion from "./component/Accordion";
 
+const items = [
+  {
+    title: "What is react?",
+    content: "React is a front ed JS framework",
+  },
+  {
+    title: "Why use react?",
+    content: "React is a favourite JS framework among developers",
+  },
+  {
+    title: "How do you use react?",
+    content: "React can be craeted by npm create-react-app",
+  },
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <br/>
+      <Accordion items={items} />
     </div>
   );
 }
