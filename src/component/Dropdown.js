f8953bf68253be0358176c45bdc26c984709ca68
harini24 <harinisiva1997@gmail.com>
2022-06-07 10:28:32 +0530

    sing useref to fix the open issue

--- src/component/Dropdown.js
+++ src/component/Dropdown.js
@@ -1,15 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Dropdown = ({ options, selectedOption, ondropdownSelect }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
 
   //manual listeners are first called
   //react listeners are called from most chil and up to the parent
   useEffect(() => {
     document.addEventListener(
       "click",
-      () => {
-        console.log("body click"); //1
+      (event) => {
+        if (ref.current.contains(event.target)) {
+          return;
+        }
+        // console.log("body click"); //1
         setOpen(false);
       },
       { capture: true }
@@ -27,21 +31,22 @@ const Dropdown = ({ options, selectedOption, ondropdownSelect }) => {
         value={option.value}
         onClick={() => {
           ondropdownSelect(option);
-          console.log("item click"); //2
+          // console.log("item click"); //2
         }}
       >
         {option.label}
       </div>
     );
   });
+
   return (
-    <div className="ui form">
+    <div className="ui form" ref={ref}>
       <div className="field">
         <label className="label">Select a color</label>
         <div
           onClick={() => {
             setOpen(!open);
-            console.log("dropdwon click"); //3
+            // console.log("dropdwon click"); //3
           }}
           className={`ui selection dropdown ${open ? "visible active" : ""}`}
         >
