7287f57e2c59ef58675f1977b918729bf6a3a7d1
harini24 <harinisiva1997@gmail.com>
2022-06-07 13:24:46 +0530

    handling command click

--- src/component/Link.js
+++ src/component/Link.js
@@ -1,10 +1,13 @@
 const Link = ({ className, href, children }) => {
   const onClick = (e) => {
+    if (e.metaKey || e.ctrlKey) {
+      return;
+    }
     e.preventDefault();
     window.history.pushState({}, "", href);
 
-    const navevent = new PopStateEvent('popstate');
-    window.dispatchEvent(navevent)
+    const navevent = new PopStateEvent("popstate");
+    window.dispatchEvent(navevent);
   };
 
   return (
